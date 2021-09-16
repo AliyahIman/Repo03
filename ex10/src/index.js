652c881778312867eb2cbd94cace3a390ac302c3
AliyahIman <90403246+AliyahIman@users.noreply.github.com>
2021-09-16 04:22:04 +0200

    Added index.js

--- ex10/src/index.js
+++ ex10/src/index.js
@@ -1,1 +1,11 @@
+function main (numOne, numTwo) {
+    //Only change code below this line
+    myRes = 15 * 3; //assigned 7
+    //Only change code above this line
+    return product;
+}
+ 
+console.log(main(15, 3));
 
+module.exports = main;
+ 
