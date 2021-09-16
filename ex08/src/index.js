903364d8d6e15b524e3e61ec30a4a9ad521814ea
AliyahIman <90403246+AliyahIman@users.noreply.github.com>
2021-09-16 04:14:19 +0200

    Added index.js

--- ex08/src/index.js
+++ ex08/src/index.js
@@ -1,1 +1,10 @@
+function main (numOne, numTwo) {
+    //Only change code below this line
+    myRes = 15 + 8; //assigned 23
+    //Only change code above this line
+    return 23;
+}
+ 
+console.log(main(15, 8));
 
+module.exports = main;
