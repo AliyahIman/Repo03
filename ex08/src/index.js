57c67e1535349ac51ff9939cb4296e52b92c03d3
AliyahIman <90403246+AliyahIman@users.noreply.github.com>
2021-09-16 04:17:35 +0200

    Correction

--- ex08/src/index.js
+++ ex08/src/index.js
@@ -2,7 +2,7 @@ function main (numOne, numTwo) {
     //Only change code below this line
     myRes = 15 + 8; //assigned 23
     //Only change code above this line
-    return 23;
+    return sum;
 }
  
 console.log(main(15, 8));
