03a0cbf96cb61aaa7f3d6197c6b5c89f20415878
AliyahIman <90403246+AliyahIman@users.noreply.github.com>
2021-09-16 03:04:28 +0200

    Added index.js :D

---
+++ ex04/src/index.js
@@ -0,0 +1,13 @@
+function main (); {
+    //Only change code below this line
+    var x = 10;
+    var y = 10;
+    var x = y;
+
+    // Only change code above this line return [x, y];
+
+}
+
+console.log(main());
+
+module.exports = main;
